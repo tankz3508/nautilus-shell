168fa7fceafb23d8e80928b78049ce4a5735bec8
tankz <tankz3508@proton.me>
2026-01-15 19:08:20 +0100

    Create strip.h

---
+++ include/strip.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/**
+ * Strips the "" from the input string (e.g. grep ".txt")
+ * @param str The string to strip the quotes from.
+ * @return void
+ */
+void strip_quotes(char* str);
